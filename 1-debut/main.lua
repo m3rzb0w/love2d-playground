33419005b7b2355a882878f14a8c69ed2bb8145a
m3rzb0w <80rN70c0D3@protonmail.com>
2024-02-24 02:01:18 +0000

    reboud count

--- 1-debut/main.lua
+++ 1-debut/main.lua
@@ -39,6 +39,7 @@ local text_logo = {
 }
 
 local debug_status = true
+local rebound_count = 0
 
 
 local function print_debug()
@@ -49,6 +50,11 @@ function love.keypressed(k)
     if k == "d" then
         debug_status = not debug_status
         print_debug()
+    end
+
+    if k == "r" then
+        rebound_count = 0
+        print("[info] resetting rebound")
     end
 end
 
@@ -74,11 +80,13 @@ local function check_position()
     --tile one
     if tile.x + tile.size >= SCREEN_WIDTH or tile.x <= 0 then
         tile.xspeed = tile.xspeed * -1
+        rebound_count = rebound_count + 1
         color_randomizer(tile)
     end
 
     if tile.y + tile.size >= SCREEN_HEIGHT or tile.y <= 0 then
         tile.yspeed = tile.yspeed * -1
+        rebound_count = rebound_count + 1
         color_randomizer(tile)
     end
 
@@ -87,11 +95,13 @@ local function check_position()
     --tile two
     if tile_two.x + tile_two.size >= SCREEN_WIDTH or tile_two.x <= 0 then
         tile_two.xspeed = tile_two.xspeed * -1
+        rebound_count = rebound_count + 1
         color_randomizer(tile_two)
     end
 
     if tile_two.y + tile_two.size >= SCREEN_HEIGHT or tile_two.y <= 0 then
         tile_two.yspeed = tile_two.yspeed * -1
+        rebound_count = rebound_count + 1
         color_randomizer(tile_two)
     end
 end
@@ -129,7 +139,7 @@ end
 
 function love.draw() -- draw on the screen
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("Press d for debug mode | status : " .. tostring(debug_status), 10, 10)
+    love.graphics.printf("Press d for debug mode | status : " .. tostring(debug_status).."\nRebound : "..rebound_count, 10, 10, SCREEN_HEIGHT, "left")
     love.graphics.setColor(love.math.colorFromBytes(text_logo.r, text_logo.g, text_logo.b))
     love.graphics.print(text_logo.val, text_logo.x, text_logo.y)
     draw_rect(tile)
